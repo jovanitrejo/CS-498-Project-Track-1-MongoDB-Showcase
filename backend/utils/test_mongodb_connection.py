9fc38af988a40cb68fad28fa392f4d7d2a55163f
Jovani Trejo <jtrej7@illinois.edu>
2026-05-02 13:57:03 -0500

    Added heartbeat check function to test connection to MongoDB

--- backend/utils/test_mongodb_connection.py
+++ backend/utils/test_mongodb_connection.py
@@ -1,6 +1,19 @@
-# from pymongo import 
+from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
 
 def test_connection():
     """
     This python file tests to check if the MONGODB_CONN env variable is set correctly and can be used to connect to the custom GCP MongoDB instance.
     """
+    uri: str | None = os.getenv("MONGO_URI")
+    if uri is None:
+        raise ValueError("MONGO_URI environment variable is not set")
+    client: MongoClient = MongoClient(uri)
+    try:
+        client.admin.command('ping')
+        print("Successfully connected!")
+    except Exception as e:
+        print(f"Connection failed: {e}")
+    finally:
+        client.close()
