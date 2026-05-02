34e3bec6f6c492ef3c8ff8282caf9bb790a948ad
Jovani Trejo <jtrej7@illinois.edu>
2026-05-02 13:08:07 -0500

    Initial Project Creation

---
+++ backend/utils/test_mongodb_connection.py
@@ -0,0 +1,6 @@
+# from pymongo import 
+
+def test_connection():
+    """
+    This python file tests to check if the MONGODB_CONN env variable is set correctly and can be used to connect to the custom GCP MongoDB instance.
+    """
