521781098916484332d13930b45c23781d0b80d1
Jovani Trejo <jtrej7@illinois.edu>
2026-05-03 13:46:19 -0500

    Added bootstrap, and QueryClient to main.tsx entry point.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import axios from "axios";
 
-createRoot(document.getElementById('root')!).render(
+axios.defaults.baseURL = "http://localhost:8000/";
+const queryClient: QueryClient = new QueryClient();
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </StrictMode>,
-)
+);
