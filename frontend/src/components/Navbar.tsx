df952f1569f124573bbc2ab1f55330fbe1e559b0
Jovani Trejo <jtrej7@illinois.edu>
2026-05-03 14:00:54 -0500

    Added basic react-bootstrap navbar

---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import ReactBootstrapNavbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Container from "react-bootstrap/Container";
+import { Link } from "react-router";
+
+const Navbar = (): React.JSX.Element => {
+  return (
+    <ReactBootstrapNavbar expand="lg" bg="light">
+      <Container>
+        <ReactBootstrapNavbar.Brand href="#home">
+          CS 498: Data Management in The Cloud
+        </ReactBootstrapNavbar.Brand>
+        <ReactBootstrapNavbar.Toggle aria-controls="basic-navbar-nav" />
+        <ReactBootstrapNavbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to="/">
+              Home
+            </Nav.Link>
+          </Nav>
+        </ReactBootstrapNavbar.Collapse>
+      </Container>
+    </ReactBootstrapNavbar>
+  );
+};
+
+export default Navbar;
