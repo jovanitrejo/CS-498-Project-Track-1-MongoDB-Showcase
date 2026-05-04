5340e92efa7d01988c1bf4783ca66e6173e53cfa
Jovani Trejo <jtrej7@illinois.edu>
2026-05-03 23:58:38 -0500

    Updated styling for Active Users

--- frontend/src/routes/MostActiveUsers.tsx
+++ frontend/src/routes/MostActiveUsers.tsx
@@ -5,6 +5,7 @@ import { getMostActiveUsers } from '../api/queryAPI';
 import Spinner from "../components/Spinner";
 import AlertError from "../components/AlertError";
 import type ActiveUserResponse from "../models/activeUserResponse";
+import Card from "react-bootstrap/Card";
 
 /**
  * Displays a table of the most active users.
@@ -21,27 +22,40 @@ const MostActiveUsers = (): React.JSX.Element => {
 
     return (
         <Container className="py-4">
-            <h2 className="mb-4">Most Active Users</h2>
-            <Table striped hover responsive>
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>Username</th>
-                        <th>Screen Name</th>
-                        <th>Tweet Count</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {data?.map((user) => (
-                        <tr key={user.user_id}>
-                            <td>{user.user_id}</td>
-                            <td>{user.user_name}</td>
-                            <td>{user.screen_name}</td>
-                            <td>{user.tweet_count}</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </Table>
+            <Card>
+                <Card.Body>
+                    <Card.Header
+                        className="mb-4"
+                        as="h2"
+                    >
+                        View Most Active Users
+                    </Card.Header>
+                    {data && data.length > 0 ? (
+                        <Table striped hover responsive>
+                            <thead>
+                                <tr>
+                                    <th>ID</th>
+                                    <th>Username</th>
+                                    <th>Screen Name</th>
+                                    <th>Tweet Count</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {data?.map((user) => (
+                                    <tr key={user.user_id}>
+                                        <td>{user.user_id}</td>
+                                        <td>{user.user_name}</td>
+                                        <td>{user.screen_name}</td>
+                                        <td>{user.tweet_count}</td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </Table>
+                    ) : (
+                        <p>No engagement data available...</p>
+                    )}
+                </Card.Body>
+            </Card>
         </Container>
     )
 }
