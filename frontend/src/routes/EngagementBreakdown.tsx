ee5fa5bfccab32f76e203483d8f3ee3562d19a9b
Jovani Trejo <jtrej7@illinois.edu>
2026-05-03 23:58:04 -0500

    Reformatted using 4-space indents

--- frontend/src/routes/EngagementBreakdown.tsx
+++ frontend/src/routes/EngagementBreakdown.tsx
@@ -12,58 +12,58 @@ import AlertError from "../components/AlertError";
  * @returns A React JSX element that represents the engagement breakdown for verified users.
  */
 const EngagementBreakdown = (): React.JSX.Element => {
-  const { data, error, isLoading } = useQuery<EngagementBreakdownResponse[]>({
-    queryKey: ["engagementBreakdown"],
-    queryFn: getEngagementBreakdown,
-  })
+    const { data, error, isLoading } = useQuery<EngagementBreakdownResponse[]>({
+        queryKey: ["engagementBreakdown"],
+        queryFn: getEngagementBreakdown,
+    })
 
-  if (isLoading) return <Spinner />
-  if (error) return <AlertError error={error} />
+    if (isLoading) return <Spinner />
+    if (error) return <AlertError error={error} />
 
-  return (
-    <Container className="py-4">
-      <Card>
-        <Card.Body>
-          <Card.Header
-            as="h2"
-            className="mb-4"
-          >
-            Engagement Breakdown by User
-          </Card.Header>
-          {data && data.length > 0 ? (
-            <Table striped hover responsive>
-              <thead>
-                <tr>
-                  <th>User Name</th>
-                  <th>Screen Name</th>
-                  <th>Total Tweets</th>
-                  <th>Simple %</th>
-                  <th>Retweet %</th>
-                  <th>Quote %</th>
-                  <th>Reply %</th>
-                </tr>
-              </thead>
-              <tbody>
-                {data.map((item) => (
-                  <tr key={item.screen_name}>
-                    <td>{item.user_name}</td>
-                    <td>{item.screen_name}</td>
-                    <td>{item.total_tweets}</td>
-                    <td>{item.simple_percent.toFixed(2)}%</td>
-                    <td>{item.retweet_percent.toFixed(2)}%</td>
-                    <td>{item.quote_percent.toFixed(2)}%</td>
-                    <td>{item.reply_percent.toFixed(2)}%</td>
-                  </tr>
-                ))}
-              </tbody>
-            </Table>
-          ) : (
-            <p>No engagement data avialable!</p>
-          )}
-        </Card.Body>
-      </Card>
-    </Container>
-  )
+    return (
+        <Container className="py-4">
+            <Card>
+                <Card.Body>
+                    <Card.Header
+                        as="h2"
+                        className="mb-4"
+                    >
+                        Engagement Breakdown by User
+                    </Card.Header>
+                    {data && data.length > 0 ? (
+                        <Table striped hover responsive>
+                            <thead>
+                                <tr>
+                                    <th>User Name</th>
+                                    <th>Screen Name</th>
+                                    <th>Total Tweets</th>
+                                    <th>Simple %</th>
+                                    <th>Retweet %</th>
+                                    <th>Quote %</th>
+                                    <th>Reply %</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {data.map((item) => (
+                                    <tr key={item.screen_name}>
+                                        <td>{item.user_name}</td>
+                                        <td>{item.screen_name}</td>
+                                        <td>{item.total_tweets}</td>
+                                        <td>{item.simple_percent.toFixed(2)}%</td>
+                                        <td>{item.retweet_percent.toFixed(2)}%</td>
+                                        <td>{item.quote_percent.toFixed(2)}%</td>
+                                        <td>{item.reply_percent.toFixed(2)}%</td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </Table>
+                    ) : (
+                        <p>No engagement data avialable!</p>
+                    )}
+                </Card.Body>
+            </Card>
+        </Container>
+    )
 }
 
 export default EngagementBreakdown;
